9ff97f2222182da0732e4d4908fe720e3046d150
Sanambir Singh <sanambir123@gmail.com>
2023-02-24 12:53:26 +0530

    setup project and folder structure

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../assets/scss/App.scss'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  )
+}
+
+export default App
