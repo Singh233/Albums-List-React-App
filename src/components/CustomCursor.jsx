b81b704831bf21e8314d6196c0a7db42dbb8c60d
Sanambir Singh <sanambir123@gmail.com>
2023-02-26 14:01:53 +0530

    add animation to movement of custom cursor

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,11 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { MouseContext } from '../context/mouse-context';
 import useMousePosition from '../hooks/useMousePosition';
 import '../styles/customCursor.css';
+import 'animate.css';
 
 const CustomCursor = () => {
     const { cursorType } = useContext(MouseContext);
     const { x, y } = useMousePosition();
+
+    useEffect(() => {
+        let timeout = null;
+            if (cursorType === 'hovered') {
+                timeout = setTimeout(() => {
+                    document.getElementsByClassName('dot')[0].classList.remove('temp');
+                    document.getElementsByClassName('dot')[0].classList.add('temp');
+                }, 500);
+            } else { 
+                document.getElementsByClassName('dot')[0].classList.remove('shrink');
+                document.getElementsByClassName('dot')[0].classList.add('shrink');
+                document.getElementsByClassName('dot')[0].classList.remove('temp');
+
+            }
+            
+        
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [cursorType]);
+
+    
     return (
         <>
             {/* <div
@@ -13,8 +36,9 @@ const CustomCursor = () => {
                 className={`ring ${cursorType}`}
             ></div> */}
             <div
-                className={`dot animate__animated animate__faster ${cursorType === 'hovered' ? 'hovered  animate__bounceIn' : ''}}`}
+                className={`dot animate__animated  ${cursorType === 'hovered' ? 'hovered '  : ''}`}
                 style={{ left: `${x}px`, top: `${y}px` }}
+                
             >
                 <div className="dotInner">
                     {cursorType === 'hovered' && <p>
